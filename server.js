e3b8c688dcb4b8a333d83c5aa9c889bb74d46e51
filippo16 <filippo.lutterotti2@gmail.com>
2024-03-02 20:34:05 +0100

    .

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = 5000;
 app.use(express.static('dist'));
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://0.0.0.0:${port}`);
 });
